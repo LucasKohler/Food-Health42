d452e11de215e8b4e39c91a557e75c23d86e97d3
Lucas Kohler <lucaskohlermarqueswork@gmail.com>
2020-09-25 17:07:59 -0300

    terminada a configuraçao do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const devMode = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
+	mode: devMode ? 'development' : 'production',
 	entry: './src/js/app.js',
 	output: {
 		filename: 'main.js',
 		path: __dirname + '/public',
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+			}),
+			new OptimizeCSSAssetsPlugin({}),
+		],
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
@@ -18,6 +30,25 @@ module.exports = {
 			{
 				test: /\.s?[ac]ss$/,
 				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: { presets: ['@babel/preset-env'] },
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'images',
+						},
+					},
+				],
 			},
 		],
 	},
